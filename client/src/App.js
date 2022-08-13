e28bc1093b6a967f9c728065525ae1bbe66819c8
maticamisay <maticamisay99@hotmail.com>
2022-08-13 01:54:21 -0300

    home button funcionality

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,16 @@ import { Landing } from './componentes/Landing';
 import { Home } from './componentes/Home';
 import { CreateRecipe } from './componentes/CreateRecipe';
 import { DetailRecipe } from './componentes/DetailRecipe';
-import LandingPage from './componentes/LandingPage';
 
 export default function App() {
   return (
     <BrowserRouter>
       <div>
         <Switch>
-          <Route path='/' component={Landing} />
-          {/* <Route path='/' component={LandingPage} /> */}
-
-          <Route path='/home' component={Home} />
-          <Route path='/create' component={CreateRecipe} />
-          <Route path='/detail/:id' component={DetailRecipe} />
+          <Route exact path='/' component={Landing} />
+          <Route exact path='/home' component={Home} />
+          <Route exact path='/create' component={CreateRecipe} />
+          <Route exact path='/detail/:id' component={DetailRecipe} />
         </Switch>
       </div>
     </BrowserRouter>
--- client/src/componentes/Landing.jsx
+++ client/src/componentes/Landing.jsx
@@ -1,30 +1,37 @@
-import { useHistory } from 'react-router-dom';
-import '../style/Landing.css';
-import video from '../assets/videoFoodCortado.mp4';
+import { Link, useHistory } from "react-router-dom";
+import "../style/Landing.css";
+import video from "../assets/videoFoodCortado.mp4";
 
 export const Landing = () => {
-   const history = useHistory();
+  const history = useHistory();
 
-   const handleChange = (e) => {
-      e.preventDefault(e);
-   }
-console.log(process.env.PUBLIC_URL + '/video.mp4');
-   return (
-      <div className="container-fluid" onChange={(e) => handleChange(e)}>
-         <div className="row home">
-            <div className="col-md-12 splash">
-               <div>
-                  <div>
-                     <h1 className='tituloPagina'>Una experiencia gustativa unica</h1>
-                  </div>
-                  <button href='/home/' className="btn btn-default CTA" onClick={() => { history.push('/home') }}>Menu</button>
-               </div>
+  const handleChange = (e) => {
+    e.preventDefault(e);
+  };
+  console.log(process.env.PUBLIC_URL + "/video.mp4");
+  return (
+    <div className="container-fluid" onChange={(e) => handleChange(e)}>
+      <div className="row home">
+        <div className="col-md-12 splash">
+          <div>
+            <div>
+              <h1 className="tituloPagina">Una experiencia gustativa unica</h1>
             </div>
-         </div>
-         <video autoPlay loop muted id="background-video">
-            <source src={video}
-               type='video/mp4' />
-         </video>
+            <Link to={'/home'}>
+              <button
+                
+                className="btn btn-default CTA"
+                // onClick={() => { history.push('/home')}}
+              >
+                Menu
+              </button>
+            </Link>
+          </div>
+        </div>
       </div>
-   )
-}
+      <video autoPlay loop muted id="background-video">
+        <source src={video} type="video/mp4" />
+      </video>
+    </div>
+  );
+};
