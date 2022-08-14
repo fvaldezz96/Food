65cf00e58daa6a8f4e83d6c1b58ca07ad4103f38
fvaldezz96 <valdezfede21@gmail.com>
2022-08-14 18:16:58 +0200

    😎

--- client/src/componentes/Landing.jsx
+++ client/src/componentes/Landing.jsx
@@ -1,37 +1,34 @@
-import { Link, useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "../style/Landing.css";
 import video from "../assets/videoFoodCortado.mp4";
 
 export const Landing = () => {
-  const history = useHistory();
 
-  const handleChange = (e) => {
-    e.preventDefault(e);
-  };
-  console.log(process.env.PUBLIC_URL + "/video.mp4");
-  return (
-    <div className="container-fluid" onChange={(e) => handleChange(e)}>
-      <div className="row home">
-        <div className="col-md-12 splash">
-          <div>
-            <div>
-              <h1 className="tituloPagina">Una experiencia gustativa unica</h1>
+   const handleChange = (e) => {
+      e.preventDefault(e);
+   };
+   console.log(process.env.PUBLIC_URL + "/video.mp4");
+   return (
+      <div className="container-fluid" onChange={(e) => handleChange(e)}>
+         <div className="row home">
+            <div className="col-md-12 splash">
+               <div>
+                  <div>
+                     <h1 className="tituloPagina">Una experiencia gustativa unica</h1>
+                  </div>
+                  <Link to={'/home'}>
+                     <button
+                        className="btn btn-default CTA"
+                     >
+                        Menu
+                     </button>
+                  </Link>
+               </div>
             </div>
-            <Link to={'/home'}>
-              <button
-                
-                className="btn btn-default CTA"
-                // onClick={() => { history.push('/home')}}
-              >
-                Menu
-              </button>
-            </Link>
-          </div>
-        </div>
+         </div>
+         <video autoPlay loop muted id="background-video">
+            <source src={video} type="video/mp4" />
+         </video>
       </div>
-      <video autoPlay loop muted id="background-video">
-        <source src={video} type="video/mp4" />
-      </video>
-    </div>
-  );
+   );
 };
