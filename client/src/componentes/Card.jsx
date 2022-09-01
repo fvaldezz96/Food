637a3c62a661483023a665767203010fbb589840
fvaldezz96 <valdezfede21@gmail.com>
2022-09-01 04:49:27 +0200

    😎ultimoCambio

--- client/src/componentes/Card.jsx
+++ client/src/componentes/Card.jsx
@@ -5,11 +5,13 @@ export const Card = ({ image, name, diets, healthScore }) => {
   // const dietas = diets.map(e => e.name)
   return (
     <div className='card'>
+      <div >
+        <img className='imagen' src={image} alt="img not fond" />
+      </div>
       <h1 className='name'>{name}</h1>
-      <img className='imagen' src={image} alt="img not fond" />
       <div>
-        <h4 className='diet'>{diets.join(', ')}</h4>
-        <h5 className='nivel'>{healthScore}</h5>
+        <h4 className='diet'>Diets: {diets.join(', ')}</h4>
+        <h5 className='nivelSaludable'>Level: {healthScore}</h5>
       </div>
     </div>
   )
